d66805c139e0687c3b9dcea42b50fda742d043ac
Gustavo Azevedo <Gustavo.Peixoto.de.Azevedo@gmail.com>
2020-09-12 11:37:38 -0300

    FrogJmp

--- PermMissingElem/PermMissingElem.js
+++ PermMissingElem/PermMissingElem.js
@@ -1,15 +1,15 @@
 function permMissingElemSet(integers) {
   const all = new Set(integers);
 
-  for (let index = 0; index <= integers.length; index++) {
+  for (let index = 1; index <= integers.length + 1; index++) {
     if (!all.has(index)) return index;
   }
 }
 
 function permMissingElemXor(integers) {
   return integers.reduce(
-    (acc, elem, index) => (acc = acc ^ elem ^ index),
-    integers.length
+    (acc, elem, index) => (acc = acc ^ elem ^ (index + 1)),
+    integers.length + 1
   );
 }
 
--- PermMissingElem/PermMissingElem.test.js
+++ PermMissingElem/PermMissingElem.test.js
@@ -2,32 +2,44 @@ const { permMissingElemSet, permMissingElemXor } = require("./PermMissingElem");
 
 describe("permMissingElem Set", () => {
   it("should work with an array with one element", () => {
-    expect(permMissingElemSet([1])).toEqual(0);
+    expect(permMissingElemSet([2])).toEqual(1);
   });
 
   it("should work when the last is missing", () => {
-    expect(permMissingElemSet([0, 1])).toEqual(2);
+    expect(permMissingElemSet([1, 2])).toEqual(3);
   });
 
   it("should work when the first is missing", () => {
-    expect(permMissingElemSet([2, 1])).toEqual(0);
+    expect(permMissingElemSet([3, 2])).toEqual(1);
   });
 
   it("should work when a middle integer is missing", () => {
-    expect(permMissingElemSet([2, 3, 0])).toEqual(1);
+    expect(permMissingElemSet([3, 4, 1])).toEqual(2);
+  });
+
+  it("should work when a middle integer is missing", () => {
+    expect(permMissingElemSet([2, 3, 1, 5])).toEqual(4);
   });
 });
 
 describe("permMissingElem Xor", () => {
   it("should work with an array with one element", () => {
-    expect(permMissingElemXor([1])).toEqual(0);
+    expect(permMissingElemXor([2])).toEqual(1);
   });
 
   it("should work when the last is missing", () => {
-    expect(permMissingElemXor([2, 1])).toEqual(0);
+    expect(permMissingElemXor([1, 2])).toEqual(3);
+  });
+
+  it("should work when the first is missing", () => {
+    expect(permMissingElemXor([3, 2])).toEqual(1);
   });
 
   it("should work when a middle integer is missing", () => {
-    expect(permMissingElemXor([2, 3, 0])).toEqual(1);
+    expect(permMissingElemXor([3, 4, 1])).toEqual(2);
+  });
+
+  it("should work when a middle integer is missing", () => {
+    expect(permMissingElemXor([2, 3, 1, 5])).toEqual(4);
   });
 });
